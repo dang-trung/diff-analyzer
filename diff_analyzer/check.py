ecc674acab1689c41ac616cd85fd6c82f89bb26f
Trung Dang <dangtrung96@gmail.com>
2023-05-29 16:17:09 +0700

    Update statistics info (missing, inf, zero).

--- diff_analyzer/check.py
+++ diff_analyzer/check.py
@@ -2,9 +2,12 @@ import tomllib
 from pathlib import Path
 
 import pandas as pd
+from pandas.api.types import is_datetime64_dtype
 from bsc_utils.database import Database, query
 
-from .utils import num_similarity, str_similarity, severity
+from .utils import (
+    num_similarity, str_similarity, severity, adjust_exchange_label, stats
+)
 
 
 def get_query(table: str) -> dict:
@@ -18,7 +21,10 @@ def get_data(query_by_db: dict) -> dict:
     oracle = query(Database.ORACLE, query_by_db.get('ORACLE'))
     mssql = query(Database.MSSQL, query_by_db.get('MSSQL'))
 
-    return {'ORACLE': pd.DataFrame(oracle), 'MSSQL': pd.DataFrame(mssql)}
+    return {
+        'ORACLE': pd.DataFrame(oracle),
+        'MSSQL': adjust_exchange_label(pd.DataFrame(mssql))
+    }
 
 
 def check(table: str) -> None:
@@ -34,13 +40,14 @@ def check(table: str) -> None:
         how='inner'
     )
 
-    print(f"{'-' * 10}{table}{'-' * 10}\nSimilarity by Column\n")
+    print(f"{'-' * 10}{table}{'-' * 10}\n")
     check_cols = [
         c for c in data.get('ORACLE').columns if c not in ignored_cols
     ]
     c_dtypes = col_dtypes(check_cols, data.get('ORACLE'))
 
     for c in check_cols:
+        stats(data.get('ORACLE'), c)
         if c in c_dtypes.get('numerical'):
             pct_similar = merge.apply(
                 lambda r: num_similarity(r[f'{c}_O'], r[f'{c}_M']), axis=1
@@ -51,12 +58,14 @@ def check(table: str) -> None:
                 lambda r: str_similarity(r[f'{c}_O'], r[f'{c}_M']), axis=1
             ).mean()
 
-        severity(c, pct_similar)
+        severity(c, pct_similar, join_cols, merge)
 
 
 def col_dtypes(cols: list, df: pd.DataFrame) -> dict:
     dtypes = {}
     dtypes['numerical'] = [c for c in cols if df[c].dtype == 'float64']
-    dtypes['text'] = [c for c in cols if df[c].dtype == 'O']
+    dtypes['text'] = [
+        c for c in cols if (df[c].dtype == 'O' or is_datetime64_dtype(df[c]))
+    ]
 
     return dtypes
--- diff_analyzer/utils.py
+++ diff_analyzer/utils.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+
+
 def num_similarity(x: float, y: float) -> float:
     if y == 0:
         if x == 0:
@@ -9,7 +13,8 @@ def num_similarity(x: float, y: float) -> float:
             return 0
         elif x != 0:
             return 1 - abs((x - y) / ((x + y) / 2))
-        
+
+
 def str_similarity(x: str, y: str) -> float:
     if x == y:
         return 1
@@ -17,7 +22,9 @@ def str_similarity(x: str, y: str) -> float:
         return 0
 
 
-def severity(c: str, pct_similar: float) -> None:
+def severity(
+    c: str, pct_similar: float, join_cols: list, df: pd.DataFrame
+) -> None:
     if 1.01 >= pct_similar > 0.99:
         severity = None
     elif pct_similar > 0.9:
@@ -26,7 +33,40 @@ def severity(c: str, pct_similar: float) -> None:
         severity = ' **'
     else:
         severity = ' ***'
-        
+
     if severity:
-        print(f'{c}: {pct_similar * 100:.1f}%{severity}')
-        
+        sample = df[df[f'{c}_O'] != df[f'{c}_M']][
+            join_cols + [f'{c}_O', f'{c}_M']].sample(1).to_dict()
+        print(f'DUPLICATES: {pct_similar * 100:.1f}%{severity} - e.g. {sample}\n')
+
+
+def adjust_exchange_label(df: pd.DataFrame) -> None:
+    if 'EXCHANGE_CODE' in df.columns:
+        df = df.replace(
+            {'EXCHANGE_CODE': {
+                'HOSTC': 'HOSE',
+                'HASTC': 'HNX',
+                'OTH': 'OTC'
+            }}
+        )
+
+    return df
+
+
+def stats(df: pd.DataFrame, col: str) -> float:
+    c = df[col]
+    num_obs = len(c)
+    if c.dtype == 'float64':
+        col_stats = {
+            'MISSING': c.isna().sum() / num_obs,
+            'INFINITE': np.isinf(c).sum() / num_obs,
+            'ZERO': (c == 0).sum() / num_obs,
+        }
+    else:
+        col_stats = {
+            'MISSING': c.isna().sum() / num_obs,
+        }
+    
+    col_stats = ' - '.join([f'{k}: {v * 100:.1f}%' for k, v in col_stats.items()])
+    
+    print(f'[{col}]\n{col_stats}')
